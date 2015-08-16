de9f70c9d43d1937a9f53fcb77787f2268248c47
tkling <tyling@gmail.com>
2015-08-16 05:39:07 -0400

    might work now

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,8 +4,8 @@ class AuthController < ApplicationController
   def via_steam_openid
     steam_oid_service = OpenID::OpenIDServiceEndpoint.from_op_endpoint_url('http://steamcommunity.com/openid/login')
     consumer = OpenID::Consumer.new(session, OpenID::Store::Memory.new)
-    auth_request = consumer.begin_without_discovery(steam_oid_service, true, true)
-    redirect_to auth_request.redirect_url('www.backlogtracker.com', 'localhost:3000/logon/success')
+    auth_request = consumer.begin_without_discovery(steam_oid_service, true)
+    redirect_to auth_request.redirect_url('http://www.backlogtracker.com', 'http://www.backlogtracker.com/logon/success')
   end
 
   def result
