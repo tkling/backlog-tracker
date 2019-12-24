c14e5f85deb650d7141231da29197c39166bac54
tkling <tyling@gmail.com>
2019-12-24 01:35:42 -0500

    underscore for unused block param

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
+require 'virtus/model'
+
 class Game
   include Virtus.model
 
   attribute :name, String
   attribute :appid, Integer
   attribute :playtime_forever, Integer, default: 0
-  attribute :playtime_forever_hours, Float, default: lambda { |game, attr| (Float(game.playtime_forever) / 60).round(2) }
+  attribute :playtime_forever_hours, Float, default: lambda { |game, _| (Float(game.playtime_forever) / 60).round(2) }
 end
