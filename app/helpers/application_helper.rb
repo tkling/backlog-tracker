257ce49bd1c0d5914f881a53a6d35d447b22ed92
tkling <tyling@gmail.com>
2019-12-24 01:38:05 -0500

    flesh out ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+  def steam_profile_url
+    session[:profile_url]
+  end
+
+  def steam_id
+    session[:steam_id]
+  end
+
+  def steam_persona_name
+    session[:persona_name]
+  end
+
+  def steam_display_name
+    # include vanity name from profile url here?
+    steam_persona_name || steam_id
+  end
+
+  def logged_in?
+    session.key?(:steam_id)
+  end
 end
