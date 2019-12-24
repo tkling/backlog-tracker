7243e5cc542eb3b9bc21bf7b8ad9ba707f9ef10b
tkling <tyling@gmail.com>
2019-12-24 01:37:08 -0500

    add logout route, use session for things

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -11,14 +11,34 @@ class AuthController < ApplicationController
     claimed_id_url = params['openid.claimed_id']
 
     if claimed_id_url && !(matches = claimed_id_url.scan(/[\d]{17}/)).empty?
-      redirect_to user_games_url(matches.first)
+      steam_id = matches.first
+      write_session_data(steam_id)
+      redirect_to user_games_url(steam_id)
     else
+      wipe_session
       redirect_to :auth_start
     end
   end
 
+  def logout
+    wipe_session
+    redirect_to :home
+  end
+
   private
+
   def base_url
     "http://#{request.env['HTTP_HOST']}"
+  end
+
+  def write_session_data(steam_id)
+    user_summary = Steam::User.summary(steam_id)
+    session[:steam_id] = steam_id
+    session[:persona_name] = user_summary['personaname']
+    session[:profile_url] = user_summary['profileurl']
+  end
+
+  def wipe_session
+    session.clear
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'home#index',                                    as: :home
   get ':user_id/games'      => 'user#games',            as: :user_games
   get 'instructions'        => 'user#instructions',     as: :instructions
   get 'auth/start'          => 'auth#via_steam_openid', as: :auth_start
   get 'auth/result'         => 'auth#result',           as: :auth_result
   get 'about'               => 'home#about',            as: :about
-  root 'home#index',                                    as: :home
+  get 'logout'              => 'auth#logout',           as: :logout
 end
